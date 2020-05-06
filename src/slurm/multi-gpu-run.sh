0ad1232b584468d1faef3d94a21e03c69804888e
SorteKanin <sortekanin@gmail.com>
2020-05-06 14:47:02 +0200

    New slurm script

---
+++ src/slurm/multi-gpu-run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# normal cpu stuff: allocate cpus, memory
+#SBATCH --ntasks=1 --cpus-per-task=4 --mem=6000M
+# we run on the gpu partition and we allocate 1 titan x
+#SBATCH -p gpu --gres=gpu:titanrtx:4
+#Note that a program will be killed once it exceeds this time!
+#SBATCH --time=7-00:00:00
+
+# Info:
+date -Is
+hostname
+echo "GPU IDs: $CUDA_VISIBLE_DEVICES"
+echo "Program output follows:"
+echo ""
+
+# Script:
+# -u: Unbuffered output
+python3 -u "$@"
+
+# End
+echo ""
+echo "Program finished"
+date -Is
