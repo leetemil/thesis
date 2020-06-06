67930ac6ac60933794fee4456f3735ebd17020f0
SorteKanin <sortekanin@gmail.com>
2020-06-06 16:25:04 +0200

    Added uniref preprocessing files

---
+++ src/data/files/uniref/prepreprocess_uniref.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from Bio import SeqIO
+
+source_file = Path("uniref50.fasta")
+target_file = open(Path("uniref50_short.fasta"), "w")
+seqs = SeqIO.parse(source_file, "fasta")
+
+count = 0
+for i, seq in enumerate(seqs):
+	if i % 1000000 == 0:
+		print(f"{i}")
+	if len(seq) <= 2000:
+		SeqIO.write(seq, target_file, "fasta")
+		count += 1
+
+print(f"Count: {count}")
+target_file.close()
